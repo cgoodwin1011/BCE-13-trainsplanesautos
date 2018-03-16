3beba0eca5e11f067753714098e3859843d99109
Charles Goodwin <cgoodwin2127@gmail.com>
2018-03-16 12:17:55 -0400

    last nights work

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCMw3ZMN5hu39Gz2m20Jk8mOB_08RasG6g",
-    authDomain: "mymehproject.firebaseapp.com",
-    databaseURL: "https://mymehproject.firebaseio.com",
-    projectId: "mymehproject",
-    storageBucket: "mymehproject.appspot.com",
-    messagingSenderId: "700669945958"
-  };
-  firebase.initializeApp(config);
+  // var config = {
+  //   apiKey: "AIzaSyCMw3ZMN5hu39Gz2m20Jk8mOB_08RasG6g",
+  //   authDomain: "mymehproject.firebaseapp.com",
+  //   databaseURL: "https://mymehproject.firebaseio.com",
+  //   projectId: "mymehproject",
+  //   storageBucket: "mymehproject.appspot.com",
+  //   messagingSenderId: "700669945958"
+  // };
+  // firebase.initializeApp(config);
